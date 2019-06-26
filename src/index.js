999048f40fbb41f2a4d5363669ec90b2a07ae479
Dylan Conway <dylan.conway567@gmail.com>
2019-06-25 21:43:40 -0700

    buggy but working

---
+++ src/index.js
@@ -0,0 +1,88 @@
+import { Universe, Cell } from 'wasm-game-of-life';
+import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg';
+import './style.css';
+
+let canvas, ctx, universe;
+let width, height;
+
+const CELL_SIZE = 4;
+
+window.onload = () => {
+
+    /* Set up the fake canvas and universe for the game */
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    
+    // width = universe.width();
+    // height = universe.height();
+
+    width = Math.floor(innerWidth / CELL_SIZE);
+    height = Math.floor(innerHeight / CELL_SIZE);
+
+    width -= width % CELL_SIZE;
+    height -= height % CELL_SIZE;
+
+    // width += innerWidth % 6;
+    // height += innerHeight % 6;
+
+    canvas.width = CELL_SIZE * width;
+    canvas.height = CELL_SIZE * height;
+
+    universe = Universe.new(width, height);
+
+    /* Call the game loop. */
+    setInterval(loop, 50);
+}
+
+const loop = () => {
+    // requestAnimationFrame(loop);
+
+    universe.tick();
+    // drawGrid();
+    drawCells();
+
+}
+
+const drawCells = () => {
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+    for (let row = 0; row < height; row ++) {
+        for (let col = 0; col < width; col ++) {
+            const idx = getIndex(row, col);
+
+            ctx.fillStyle = cells[idx] === Cell.Dead
+                ? 'black'
+                : 'white'
+            
+            ctx.fillRect(
+                col * CELL_SIZE,
+                row * CELL_SIZE,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+}
+
+const drawGrid = () => {
+    ctx.beginPath();
+    ctx.strokeStyle = 'gray';
+
+    for (let i = 0; i <= width; i ++) {
+        ctx.moveTo(i * CELL_SIZE, 0);
+        ctx.lineTo(i * CELL_SIZE, CELL_SIZE * height);
+    }
+
+    for (let i = 0; i <= height; i ++) {
+        ctx.moveTo(0, i * CELL_SIZE);
+        ctx.lineTo(CELL_SIZE * width, i * CELL_SIZE);
+    }
+
+    ctx.stroke();
+}
+
+const getIndex = (row, col) => {
+    return row * width + col;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,29 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: "./src/bootstrap.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 5000
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    new CopyWebpackPlugin(['dist/index.html'])
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
 };
